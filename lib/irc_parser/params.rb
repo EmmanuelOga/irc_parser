b7871a5c532a37a42cb887a65da2ddef1c8224a3
Emmanuel Oga <EmmanuelOga@gmail.com>
2010-09-20 13:19:32 -0300

    simplified Params#to_s

--- lib/irc_parser/params.rb
+++ lib/irc_parser/params.rb
@@ -8,30 +8,21 @@ module IRCParser
       params.each_with_index { |elem, index| self[index] = elem }
     end
 
+    # Postfixes is the number of paremeters used to build the last parameter
     # The message has a number of postfixes, which are the paremeters to be
     # joined in the last parameter. This is because the RFC protocol only
     # allows blank on the last param.  All the parameters are joined by a
     # space.
-    # Postfixes is the number of paremeters used to build the last parameter
     def to_s(postfixes)
       return "" if empty? || (length == 1 && (first.nil? || first == ""))
 
-      parameters = reject {|elem| elem.nil? } #dup
+      parameters = reject {|elem| elem.nil? }
       parameters.pop while parameters.last == PLACEHOLDER # don't send unneeded wildcards
       parameters = parameters.map { |val| val == PLACEHOLDER ? "*" : val }
 
-      # With 0 postfixes we might still want to check if the last one has
-      # spaces, because in that case we need to put the colon in the last param
-      # even if it is not a postfix.
-      last = []
-      (postfixes.nil? || postfixes == 0 ? 1 : postfixes).times do
-        param = parameters.pop
-        last.unshift param
-      end
-
-      last = last.flatten.join(" ")
-
-      parameters.push(last == "" || last.nil? || last =~ /\s+/ || ( postfixes && postfixes > 0 ) ? ":#{last}" : last)
+      split_index = postfixes.nil? || postfixes == 0 ? 1 : postfixes
+      parameters, last = parameters[0...-split_index], parameters[-split_index..-1].flatten.join(" ")
+      parameters.push(last == "" || last.nil? || last =~ /\s+/ || (postfixes && postfixes > 0) ? ":#{last}" : last)
 
       parameters.join(" ") || ""
     end
